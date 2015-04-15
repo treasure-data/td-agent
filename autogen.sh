b72994ce4ff60b7cb9d2e6ed913f7bdf9323cfad
Masahiro Nakagawa <repeatedly@gmail.com>
2015-04-15 20:45:12 +0900

    Update ruby to latest 1.9.3 patch level

--- autogen.sh
+++ autogen.sh
@@ -29,7 +29,7 @@ mkdir -p deps/ruby
 mkdir -p plugins
 cd deps
 download "http://www.canonware.com/download/jemalloc" "jemalloc-2.2.5.tar.bz2"
-download "http://ftp.ruby-lang.org/pub/ruby/1.9" "ruby-1.9.3-p484.tar.bz2"
+download "http://ftp.ruby-lang.org/pub/ruby/1.9" "ruby-1.9.3-p551.tar.bz2"
 download "http://rubygems.org/downloads" "bundler-1.3.6.gem"
 download "http://rubygems.org/downloads" "json-1.7.7.gem"
 download "http://rubygems.org/downloads" "msgpack-0.4.7.gem"
